c4a534418aead83ccb895fc4dc24e20c09df695b
Filipa <filipa.ferreirasilva@gmail.com>
2024-10-14 14:12:11 +0100

    Functionalities completed

--- src/App.js
+++ src/App.js
@@ -13,12 +13,26 @@ export default function App(){
     setItems(items=>[...items, item])
   }
 
+  function handleDeleteItem (id){
+    setItems(items=>items.filter(item=>item.id !== id))
+  }
+
+  function handleToggleItem(id){
+    setItems((items)=>items.map((item)=>item.id===id ? {...item, packed: !item.packed}: item))
+  }
+
+  function  handleClearList(){
+    const confirmed= window.confirm('Are you sure you want to delete all items?')
+    if(confirmed) setItems([])
+
+  }
+  
   return(
     <div className="app">
   <Logo/>
   <Form onAddItems={handleAddItems}/>
-  <PackingList items={items}/>
-  <Stats/>
+  <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} onClearList={handleClearList}/>
+  <Stats items={items}/>
  </div>
   )
 
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,5 +1,17 @@
-export default function Stats (){
+export default function Stats ({items}){
+
+    if(!items.length){
+        return(
+            <p className="stats"><em>Start adding some items to your packing list 🚀</em></p>
+
+        )
+}
+    const numItems= items.length
+    const numPacked=items.filter((item)=>item.packed).length
+    const percentage= Math.round((numPacked/numItems)*100)
+
+
     return <footer className="stats"> 
-      <em> 💼You've x items on you list, and you already packed x (%x)
+      <em> {percentage === 100 ? 'You got everything! Ready to go 🛫' : `💼You've ${numItems} items on you list, and you already packed ${numPacked} (${percentage}%)`}
         </em></footer>
   }
